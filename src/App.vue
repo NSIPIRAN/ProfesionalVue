4ab72a2718b9035f149609b2ae9eeaf87cbb3d0f
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2021-05-25 15:32:13 -0500

    created project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hola bebe"/>
+    <h1>{{msg}}</h1>
+    <p>{{person.name}}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Hola mundo',
+      person: {
+        name: 'juan'
+      }
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
