27b7e717a6c557c22ca275fe350ffb1f364afb53
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2021-05-26 01:35:29 -0500

    Using Bulma and created componet in layout

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,62 @@
 <template>
   <div id="app">
+
+    <MHeader></MHeader>
     <section class="section"> 
-      <nav class="nav">
+      
         <div class="container">
           <input type="text" placeholder="Buscar canciones" v-model="searchQuery">
           <a href=""> <button v-on:click.prevent="search">Buscar</button></a>
         </div>
         <p>{{searchMessage}}</p>
-      </nav>
+      
       <div class="container">
         <div class="columns">
           <div class="column" 
             v-for="track in tracks" 
-            :key="track.artists[0].name">
+            :key="track.id">
             {{track.name}} - {{track.artists[0].name}}
           </div>
         </div>  
       </div>
+      
     </section>
+    <MFooter></MFooter>
   </div>
 </template>
-  
+
 <script>
-import trackService from  './Service/track.js'
+import trackService from  './Service/track'
+
+import MFooter from './components/layout/footer.vue'
+import MHeader from './components/layout/header.vue'
 
 export default {
-  name: 'App',
-  data() {
+  name: 'app',
+  components: { MFooter, MHeader },
+  data () {
     return {
-      searchQuery:'',
+      searchQuery: '',
       tracks: []
     }
   },
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
   methods: {
-   search(){
-     if(this.searchQuery){return}
-     trackService.search(this.searchQuery)
-      .then(res=>{
-        this.tracks = res.tracks.items
-      })
-   }
-  },
-  components: {
-  },
-  computed:{
-    searchMessage(){
-      return ` Encontrados: ${this.tracks.length}`
+    search () {
+      if (!this.searchQuery) { return }
+      trackService.search(this.searchQuery)
+        .then(res => {
+          this.tracks = res.tracks.items
+          console.log(res.tracks.items)
+        })
     }
   }
 }
 </script>
+<style lang="scss">
+  @import "./scss/main.scss";
+</style>
---
+++ src/components/layout/header.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="hero is-primary is-medium">
+   <!-- Hero head: will stick at the top -->
+  <div class="hero-head">
+    <nav class="navbar">
+      
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+          </a>
+          <div class="navbar-burger" @click="showNav = !showNav" :class="{ 'is-active': showNav }">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div :class="{ 'is-active': showNav }" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">Home</a>
+            <a class="navbar-item">Examples</a>
+            <a class="navbar-item">Documentation</a>
+          </div>
+        </div>
+     
+    </nav>
+  </div>
+
+  <!-- Hero content: will be in the middle -->
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <p class="title">
+        Gugul Music
+      </p>
+      <p class="subtitle">
+        Canciones que te trae Vue
+      </p>
+    </div>
+  </div>
+
+    
+  </section>
+</template>
+<script>
+export default {
+   data () {
+    return {
+      showNav: false
+    }
+  },
+}
+</script>
+
